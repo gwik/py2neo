bcaea33e94ae165ef4f17a1ab8c757ce005b8991
Antonin Amand <antonin.amand@gmail.com>
2012-05-02 15:22:59 +0200

    distribute compatible setup

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from pkg_resources import require
+from setuptools import setup
 
-require("tornado")
 setup(
-	name="py2neo",
-	version=file("VERSION").read(),
-	description="Python bindings to Neo4j",
-	long_description="""The py2neo project provides bindings between Python and Neo4j via its RESTful web service interface. It attempts to be both Pythonic and consistent with the core Neo4j API and is compatible with Python 3.""",
-	author="Nigel Small",
-	author_email="py2neo@nigelsmall.net",
-	url="http://py2neo.org/",
-	scripts=["scripts/cypher"],
-	package_dir={"": "src"},
-	packages=["py2neo"],
-	license="Apache License, Version 2.0",
-	classifiers=[]
+        name="py2neo",
+        version=file("VERSION").read(),
+        description="Python bindings to Neo4j",
+        long_description="""The py2neo project provides bindings between Python and Neo4j via its RESTful web service interface. It attempts to be both Pythonic and consistent with the core Neo4j API and is compatible with Python 3.""",
+        author="Nigel Small",
+        author_email="py2neo@nigelsmall.net",
+        url="http://py2neo.org/",
+        scripts=["scripts/cypher"],
+        package_dir={"": "src"},
+        packages=["py2neo"],
+        license="Apache License, Version 2.0",
+        classifiers=[],
+        install_requires=[
+            'setuptools',
+            'geventhttpclient']
 )
